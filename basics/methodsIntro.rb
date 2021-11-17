411d6b5f7b018513cd05935ddcadb22a9a2cbde2
jaderiver62 <jaderiver64@gmail.com>
2021-11-17 17:30:32 -0600

    methods

---
+++ basics/methodsIntro.rb
@@ -0,0 +1,18 @@
+def prime?(number)
+    Prime.prime?(number)
+end
+
+# same as
+
+def prime?(number)
+    return true if number == 2 || number == 3
+    return false if number == 1 || number % 2 == 0 || number % 3 == 0 
+    i = 5
+    j = 2
+    while i*i <= number
+        return false if number % i == 0
+        i += j
+        j = 6 - j
+    end
+    return true
+end
