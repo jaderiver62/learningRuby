a801ac89fd9e4d0156f087ce240233cf5ff6e8c7
jaderiver62 <jaderiver64@gmail.com>
2021-11-16 23:19:06 -0600

    enums

--- basics/enumsIntro.rb
+++ basics/enumsIntro.rb
@@ -1,4 +1,4 @@
 def sum_terms(n)
     (1..n).reduce(0) {|sum, val| sum += (val * val + 1)}
-   END
+end
        # For a series of non-negative integers up to n, compute the sum of all terms after each has been squared and 1 added.
